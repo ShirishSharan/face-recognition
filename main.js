f9e924ac5d80be983b79df618477a4ac48072f71
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-04-05 19:11:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 function setup(){
-    canvas=createCanvas(300, 300);
-    canvas.position(200,400);
+    canvas=createCanvas(400, 385);
+    canvas.position(450,200);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/GM4qcRll4/model.json' , modelLoaded);
 }
 
 function draw(){
-
+image(video, 0 ,0 ,400 ,385);
+classifier.classify(video, gotResult);
 }
+
+function gotResult(error, result){
+    if(error)
+    {console.log(error);
+    }else{
+    console.log(result);
+    document.getElementById("result_object_name").innerHTML = result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+    }
+
+    
+}
+
+function modelLoaded(){
+    console.log('model loaded !')
+}
+
+
+
 
